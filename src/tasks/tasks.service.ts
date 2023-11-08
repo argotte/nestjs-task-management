06899b3ee4be527947359883009f412a0557c6d2
diegoargottez@gmail.com <diegoargottez@gmail.com>
2023-11-08 01:26:46 -0400

    [imp]: error handling on delete and get

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -12,8 +12,13 @@ export class TasksService {
     return this.tasks;
   }
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+    if (!found) {
+      throw new NotFoundException(`Task with ID ${id} not found.`);
+    }
+    return found;
   }
+
   getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
     const { status, search } = filterDto;
     let tasks = this.getAllTasks();
@@ -51,7 +56,8 @@ export class TasksService {
   }
 
   deleteTaskById(id: string): void {
-    this.tasks = this.tasks.filter((tasks) => tasks.id !== id);
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter((tasks) => tasks.id !== found.id);
   }
 
   updateTaskStatus(id: string, status: TaskStatus) {
